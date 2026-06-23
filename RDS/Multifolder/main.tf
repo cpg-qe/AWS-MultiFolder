1ec3b1e6678c28974d0589fdf2c15b207fa65fe0
cpg-qe <swathi.g@servicenow.com>
2026-06-23 19:46:12 +0530

    Update main.tf

--- RDS/Multifolder/main.tf
+++ RDS/Multifolder/main.tf
@@ -1,11 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.67.0"
+terraform {                                                                                                                                                                                                                                       
+    required_providers {                                                                                                                                                                                                                          
+      aws = {                                                                                                                                                                                                                                       
+        source  = "hashicorp/aws"                                                                                                                                                                                                                 
+        version = "~> 5.0"
+      }
     }
   }
-}
+
 
 
 provider "aws" {
